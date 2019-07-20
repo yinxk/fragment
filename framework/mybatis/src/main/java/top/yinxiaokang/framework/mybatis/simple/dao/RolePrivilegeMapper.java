79028410f83a6f81920a10e41d2fe03025ec1fa0
justdi <735761310@qq.com>
2019-07-20 22:59:43 +0800

    将生成的删除, 练习练习

--- framework/mybatis/src/main/java/top/yinxiaokang/framework/mybatis/simple/dao/RolePrivilegeMapper.java
+++
@@ -1,9 +0,0 @@
-package top.yinxiaokang.framework.mybatis.simple.dao;
-
-import top.yinxiaokang.framework.mybatis.simple.model.RolePrivilege;
-
-public interface RolePrivilegeMapper {
-    int insert(RolePrivilege record);
-
-    int insertSelective(RolePrivilege record);
-}
--- framework/mybatis/src/main/java/top/yinxiaokang/framework/mybatis/simple/dao/UserRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package top.yinxiaokang.framework.mybatis.simple.dao;
-
-import top.yinxiaokang.framework.mybatis.simple.model.UserRole;
-
-public interface UserRoleMapper {
-    int insert(UserRole record);
-
-    int insertSelective(UserRole record);
-}
