3cbce022a4f83e2bedf42df4a7843e1ca61deafd
justdi <735761310@qq.com>
2019-07-21 00:27:04 +0800

    手动练习

--- framework/mybatis/src/main/java/top/yinxiaokang/framework/mybatis/simple/dao/UserMapper.java
+++ framework/mybatis/src/main/java/top/yinxiaokang/framework/mybatis/simple/dao/UserMapper.java
@@ -2,8 +2,8 @@ package top.yinxiaokang.framework.mybatis.simple.dao;
 
 import top.yinxiaokang.framework.mybatis.simple.model.User;
 
-public interface UserMapper {
-    int deleteByPrimaryKey(Long id);
+import java.util.List;
 
-    int updateByPrimaryKey(User record);
+public interface UserMapper {
+    List<User> selectById(String id);
 }
