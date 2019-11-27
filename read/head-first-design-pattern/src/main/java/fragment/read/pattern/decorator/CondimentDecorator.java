53177aa1f390753a0d9598b4138d97fdc5a3c1d1
justdi <735761310@qq.com>
2019-11-27 21:01:17 +0800

    装饰者模式

---
+++ read/head-first-design-pattern/src/main/java/fragment/read/pattern/decorator/CondimentDecorator.java
@@ -0,0 +1,9 @@
+package fragment.read.pattern.decorator;
+
+/**
+ * 调料
+ */
+public abstract class CondimentDecorator extends Beverage {
+    @Override
+    public abstract String getDescription();
+}
