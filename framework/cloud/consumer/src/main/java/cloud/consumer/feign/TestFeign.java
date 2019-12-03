8318de38062f6fca7dc6c56e734ab9a7fc19694c
justdi <735761310@qq.com>
2019-12-03 11:28:57 +0800

    feign日志配置

--- framework/cloud/consumer/src/main/java/cloud/consumer/feign/TestFeign.java
+++ framework/cloud/consumer/src/main/java/cloud/consumer/feign/TestFeign.java
@@ -1,21 +1,22 @@
 package cloud.consumer.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import cloud.base.response.BaseResponse;
 import cloud.base.response.dto.UserInfo;
 import cloud.base.response.dto.UserInfo2;
 import cloud.base.response.dto.UserInfo3;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "provider")
+@FeignClient(value = "provider", configuration = FooConfiguration.class)
 public interface TestFeign {
 
-    @GetMapping("/info")
-    BaseResponse<UserInfo> getInfo();
+	@GetMapping("/info")
+	BaseResponse<UserInfo> getInfo();
 
-    @GetMapping("/info")
-    BaseResponse<UserInfo2> getInfo2();
+	@GetMapping("/info")
+	BaseResponse<UserInfo2> getInfo2();
 
-    @GetMapping("/info")
-    BaseResponse<UserInfo3> getInfo3();
+	@GetMapping("/info")
+	BaseResponse<UserInfo3> getInfo3();
 }
