8f69885ab4ec7115f43cf82f55014b24c1627250
hic <735761310@qq.com>
2020-08-18 23:36:19 +0800

    final

--- base/src/main/java/test/Factorial.java
+++ base/src/main/java/test/Factorial.java
@@ -79,7 +79,7 @@ public class Factorial {
         for (int j = 0; j < bValidLength; j++) {
             long cDigit = c[j] + a * b[j];
             c[j] = cDigit & MASK;
-            c[j + 1] += cDigit >> SHIFT;
+            c[j + 1] = cDigit >> SHIFT;
         }
     }
 
