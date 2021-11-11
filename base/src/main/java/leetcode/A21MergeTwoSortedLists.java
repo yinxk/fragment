099d1a114c8b5400aec8bf7dd6015857004d48a6
hic <735761310@qq.com>
2021-11-11 22:11:43 +0800

    合并两个链表

---
+++ base/src/main/java/leetcode/A21MergeTwoSortedLists.java
@@ -0,0 +1,113 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ */
+public class A21MergeTwoSortedLists {
+
+    public static void main(String[] args) {
+
+
+        A21MergeTwoSortedLists a21MergeTwoSortedLists = new A21MergeTwoSortedLists();
+        System.out.println(a21MergeTwoSortedLists.mergeTwoLists(ListNode.as(1, 2, 4), ListNode.as(1, 3, 4)));
+        System.out.println(a21MergeTwoSortedLists.mergeTwoLists(null, ListNode.as(0)));
+        System.out.println(a21MergeTwoSortedLists.mergeTwoLists(null, null));
+    }
+
+    /**
+     * 合并时, 在处理某一个链表为空的时候, 冗余了步骤
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        ListNode dummy = new ListNode(0, null);
+        ListNode cur = dummy;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val <= cur2.val) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = cur1 == null ? cur2 : cur1;
+        return dummy.next;
+    }
+
+    /**
+     * 第一步想法, 直接合并
+     */
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        ListNode dummy = new ListNode(0, null);
+        ListNode cur = dummy;
+        while (cur1 != null || cur2 != null) {
+            if (cur1 != null && cur2 != null) {
+                if (cur1.val <= cur2.val) {
+                    cur.next = cur1;
+                    cur1 = cur1.next;
+                } else {
+                    cur.next = cur2;
+                    cur2 = cur2.next;
+                }
+            } else if (cur1 != null) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.append("[");
+            ListNode temp = this;
+            boolean first = true;
+            while (temp != null) {
+                if (first) {
+                    first = false;
+                } else {
+                    strBuilder.append(",");
+                }
+                strBuilder.append(temp.val);
+                temp = temp.next;
+            }
+            strBuilder.append("]");
+            return strBuilder.toString();
+        }
+
+        public static ListNode as(int... val) {
+            ListNode pre = null;
+            ListNode head = null;
+            for (int i = val.length - 1; i >= 0; i--) {
+                head = new ListNode(val[i], pre);
+                pre = head;
+            }
+            return head;
+        }
+    }
+}
