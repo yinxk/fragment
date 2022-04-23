388c6df7cb469c7bd0125b644555f969965367ba
hic <735761310@qq.com>
2022-04-23 14:24:11 +0800

    简单dfs和bfs

--- base/src/main/java/leetcode/A695.java
+++ base/src/main/java/leetcode/A695.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class A695 {
@@ -23,7 +25,9 @@ public class A695 {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    maxArea = Math.max(maxArea, dfsLoop(grid, i, j));
+                    // maxArea = Math.max(maxArea, dfsRecursion(grid, i, j));
+                    // maxArea = Math.max(maxArea, dfsLoop(grid, i, j));
+                    maxArea = Math.max(maxArea, bfs(grid, i, j));
                 }
             }
         }
@@ -70,6 +74,31 @@ public class A695 {
             // stack.push(new int[]{a + 1, b});
             // stack.push(new int[]{a, b - 1});
             // stack.push(new int[]{a, b + 1});
+        }
+        return area;
+    }
+    public int bfs(int[][] grid, int x, int y) {
+        int area = 0;
+        int[] directions = new int[]{-1, 0, 1, 0, -1};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        while (!queue.isEmpty()) {
+            int[] point = queue.poll();
+            int a = point[0];
+            int b = point[1];
+            if (a < 0 || b < 0 || a == grid.length || b == grid[0].length ||
+                    grid[a][b] == 0) {
+                continue;
+            }
+            area++;
+            grid[a][b] = 0;
+            for (int i = 0; i < 4; i++) {
+                queue.add(new int[]{a + directions[i], b + directions[i + 1]});
+            }
+            // queue.add(new int[]{a - 1, b});
+            // queue.add(new int[]{a + 1, b});
+            // queue.add(new int[]{a, b - 1});
+            // queue.add(new int[]{a, b + 1});
         }
         return area;
     }
